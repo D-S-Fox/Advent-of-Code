4bcda6ebd74ceee36d24f91ef4562369c2f47a7f
D-S-Fox <dylan2418@gmail.com>
2025-12-06 18:01:46 -0600

    day 2 puzzle 1 done

--- Python/2025/Day_2/main.py
+++ Python/2025/Day_2/main.py
@@ -8,6 +8,21 @@ def puzzle1(data):
     start_time = perf_counter()
     puzzle1_answer = 0
 
+    for ids in data:
+        total_id_range = ids.split("-")
+        id_1 = int(total_id_range[0])
+        id_2 = int(total_id_range[1])
+
+        for i in range(id_2 - id_1 + 1):
+            current_id = str(id_1 + i)
+
+            if len(current_id) % 2 == 0:
+                midpoint = len(current_id) // 2
+                first_half = current_id[:midpoint]
+                second_half = current_id[midpoint:]
+
+                if first_half == second_half:
+                    puzzle1_answer += int(current_id)
 
     print(f'Puzzle 1 Answer: {puzzle1_answer}')
     end_time = perf_counter()
@@ -29,20 +44,20 @@ def get_input():
     dname = os.path.dirname(abspath)
     os.chdir(dname)
 
-    #with open("input.txt") as infile
-    with open("testinput.txt") as infile:
+    with open("input.txt") as infile:
+    #with open("testinput.txt") as infile:
         end_time = perf_counter()
         print(f"Got input in {end_time - start_time:.8f} seconds.\n")
-        return infile.read().splitlines()
+        return infile.read()
         
 def format_data(raw_input):
     start_time = perf_counter()
 
-
-
+    data = str(raw_input).split(",")
+    
     end_time = perf_counter()
     print(f"Formated data in {end_time - start_time:.8f} seconds.\n")
-    return ()
+    return (data)
 
 
 def main():
