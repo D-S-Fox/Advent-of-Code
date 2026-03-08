4165fca1261e94951d860a2114c18585cd9416bc
Dylan Fox <dylan2418@gmail.com>
2026-03-07 21:53:21 -0600

    TODO P2: Filter each line to +/-<int> and process

--- C++/2018/day_1/main.cpp
+++ C++/2018/day_1/main.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <algorithm>
+
+using std::cout;
 
 int main() {
     
-    std::fstream input("./input.txt", std::ios_base::in);
+    std::fstream input("./testinput.txt", std::ios_base::in);
 
-    int freq = 0;
-    int num;
-    while (input >> num) {
-        freq += num;
+    int freq {0};
+    int p2_freq {0};
+    int i {};
+
+    while (input >> i) {
+        freq += i;
     }
+    
+    cout << "Puzzle 1 - frequency: " << freq << "\n";
+    cout << "Puzzle 2 - frequency: " << p2_freq << "\n";
 
-    printf("Puzzle 1 - frequency: %d\n", freq);
+    input.close();
     return 0;
 }
