668d80a1dad925dd523ebaa499ec8b57dd9068cd
Dylan Fox <dylan2418@gmail.com>
2026-03-08 20:58:45 -0500

    fix errors

--- C++/2018/day_1/main.cpp
+++ C++/2018/day_1/main.cpp
@@ -17,14 +17,14 @@ int main() {
     int line {};
 
     while (input >> line) {
-        freq += i;
+        freq += line;
         if (p2_freq == 0 || freq_list.size() >= 1)
         {
-            std::string stg {std::to_string(i)};
+            std::string stg {std::to_string(line)};
             if (stg.substr(0, 1) == "-")
             {
                 int to_move = std::stoi(stg.substr(1, -1));
-                for ()
+                
             }
         }
         freq_list.push_back(freq);
