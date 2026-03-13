ccef9cb8b1eb65739dab0461fa3bf5b4bddfb9f0
Dylan Fox <dylan2418@gmail.com>
2026-03-12 20:54:52 -0500

    New idea for puzzle

--- C++/2018/day_1/main.cpp
+++ C++/2018/day_1/main.cpp
@@ -3,34 +3,60 @@
 #include <algorithm>
 #include <vector>
 #include <string>
-#include <typeinfo>
+#include <cstdlib>
 
 using std::cout;
 
-int main() {
-    
-    std::fstream input("./testinput.txt", std::ios_base::in);
+int puzzle_2(std::string targetfile)
+{
+    std::fstream input(targetfile, std::ios_base::in);
+    int line{};
+    int p2_freq{0};
 
-    std::vector<int> freq_list {0};
-    int freq {0};
-    int p2_freq {0};
-    int line {};
-
-    while (input >> line) {
-        freq += line;
-        if (p2_freq == 0 || freq_list.size() >= 1)
-        {
-            std::string stg {std::to_string(line)};
-            if (stg.substr(0, 1) == "-")
-            {
-                int to_move = std::stoi(stg.substr(1, -1));
-                
-            }
-        }
-        freq_list.push_back(freq);
+    while (input >> line)
+    {
+        cout << line << "\n";
     }
-    
+    return 67;
+}
+
+int main()
+{
+    std::string targetfile {"./testinput.txt"};
+    std::fstream input(targetfile, std::ios_base::in);
+
+    std::vector<int> freq_list{0};
+    int freq{0};
+    int line{};
+
+    while (input >> line)
+    {
+        freq += line;
+        freq_list.push_back(freq);
+
+    {
+        int itterate = std::abs(line);
+        int current_freq = freq;
+        for (size_t i {-1}; i > itterate; ++i)
+        {
+            if (line > -1)
+               {
+                   current_freq++;
+               }
+               else
+               {
+                   current_freq--;
+             
+
+               int count = std::count(freq_list.begin(), freq_list.end(), current_freq);
+               if (count > 0)
+                   p1_freq = current_freq;
+           }
+       }
+    }
+
     cout << "Puzzle 1 - frequency: " << freq << "\n";
+    int p2_freq = puzzle_2(targetfile);
     cout << "Puzzle 2 - frequency: " << p2_freq << "\n";
 
     input.close();
---
+++ C++/2018/day_1/main2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cstdlib>
+
+/*
+TODO:
+    - main:
+        vars
+        - target file
+        - frequency vector
+
+        call
+        - int puzzle1(file, vec)
+            populate all freqs in vec
+            return freq
+        - int puzzle2(file, vec)
+            for each number
+                parse each change by 1
+                see if freq exists in vec > once
+
+*/
